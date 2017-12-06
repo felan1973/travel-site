766375ba449666ad0251a5b20b68b6cc6198f6a5
Federico <felan1973@gmail.com>
2017-12-06 09:50:56 +0100

    Added semicolon to gulp/tasks/watch.js missed

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,7 +17,7 @@ gulp.task('watch', function(){
     
         watch('./app/assets/styles/**/*.css', function(){
             gulp.start('cssInject');
-        })
+        });
     });
 
     gulp.task('cssInject',['styles'],function() {
