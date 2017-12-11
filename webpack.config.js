9b272a7a0b74aca7de971bb205de275d9e1df4e2
Federico <felan1973@gmail.com>
2017-12-11 20:54:39 +0100

    Desktop e mobile style completato.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             {
                 loader: 'babel-loader',
                 query: {
-                    preset: ['es2015']
+                    presets: ['es2015']
                 },
                 test: /\.js$/,
                 exclude: /node_modules/
